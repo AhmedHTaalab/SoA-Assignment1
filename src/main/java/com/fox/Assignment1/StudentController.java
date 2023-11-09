a11988230f572d61bfdb91e861327c7d1432300f
AhmedHTaalab <117315193+AhmedHTaalab@users.noreply.github.com>
2023-11-09 23:51:27 +0200

    V1ButNotWorking505

--- src/main/java/com/fox/Assignment1/StudentController.java
+++ src/main/java/com/fox/Assignment1/StudentController.java
@@ -1,25 +1,64 @@
 package com.fox.Assignment1;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 @RestController
 @RequestMapping("/Student")
 public class StudentController {
 
-    @GetMapping("/")
-    public String getAllStudents(){
-       return "test test i love you";
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
-    @GetMapping("/ByGPA")
-    public String getByGPA(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @GetMapping("/TEST")
+    public List<StudentModel> getAllStudents() {
+
+        List<StudentModel> Students = new ArrayList<>();
+        Students = studentService.XMLRead();
+        return  Students;
     }
 
-    @GetMapping("/ByName")
-    public String getByName(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+
+    @GetMapping("/ByGPA/{targetGPA}")
+    public List<StudentModel> getStudentsByGPA(@PathVariable  double targetGPA) {
+        List<StudentModel> students = studentService.XMLRead(); // Assuming XMLRead returns a list of all students
+
+        List<StudentModel> studentsWithTargetGPA = new ArrayList<>();
+
+        for (StudentModel student : students) {
+            if (student.getGPA() == targetGPA) {
+                studentsWithTargetGPA.add(student);
+            }
+        }
+
+        return studentsWithTargetGPA;
+    }
+
+    @GetMapping("/ByName/{targetName}")
+    public List<StudentModel> getStudentsByName(@PathVariable  String targetName) {
+        List<StudentModel> students = studentService.XMLRead(); // Assuming XMLRead returns a list of all students
+
+        List<StudentModel> studentsWithTargetName= new ArrayList<>();
+
+        for (StudentModel student : students) {
+            if (student.getFirstname() == targetName) {
+                studentsWithTargetName.add(student);
+            }
+        }
+
+        return studentsWithTargetName;
     }
 
     @PostMapping("/Student")
--- src/main/java/com/fox/Assignment1/StudentService.java
+++ src/main/java/com/fox/Assignment1/StudentService.java
@@ -1,5 +1,70 @@
 package com.fox.Assignment1;
 
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+@Service
 public class StudentService {
 
+    public List<StudentModel> XMLRead() {
+        try{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Load the input XML document, parse it and return an instance of the
+        // Document class.
+        Document document = builder.parse(new File("D:\\College\\SOA\\JavaSpring\\Student.xml"));
+
+        List<StudentModel> Students = new ArrayList<StudentModel>();
+        NodeList nodeList = document.getDocumentElement().getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element elem = (Element) node;
+
+                // Get the value of the ID attribute.
+                String ID = node.getAttributes().getNamedItem("ID").getNodeValue();
+
+                // Get the value of all sub-elements.
+                String firstname = elem.getElementsByTagName("Firstname")
+                        .item(0).getChildNodes().item(0).getNodeValue();
+
+                String lastname = elem.getElementsByTagName("Lastname").item(0)
+                        .getChildNodes().item(0).getNodeValue();
+
+                String Gender = elem.getElementsByTagName("Gender").item(0)
+                        .getChildNodes().item(0).getNodeValue();
+
+                double GPA = Double.parseDouble(elem.getElementsByTagName("GPA")
+                        .item(0).getChildNodes().item(0).getNodeValue());
+
+                int Level = Integer.parseInt(elem.getElementsByTagName("Level")
+                        .item(0).getChildNodes().item(0).getNodeValue());
+
+                String Address = elem.getElementsByTagName("Address").item(0)
+                        .getChildNodes().item(0).getNodeValue();
+
+                Students.add(new StudentModel(ID, firstname, lastname, Gender, GPA, Level, Address));
+            }
+
+        }
+        return Students;
+    }catch (IOException | ParserConfigurationException | SAXException e) {
+            // Handle the exception or log it
+            e.printStackTrace();
+            return new ArrayList<>(); // Return an empty list or handle the error accordingly
+        }
+    }
 }
