9b49e7cf59ca33486c6bd3a130f6288ef318f0cb
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-11-10 21:07:58 +0200

    fix

--- src/main/java/com/fox/Assignment1/StudentService.java
+++ src/main/java/com/fox/Assignment1/StudentService.java
@@ -96,6 +96,8 @@ public class StudentService {
         Element studentElement = doc.createElement("Student");
         studentElement.setAttribute("ID", student.getID());
 
+        System.out.println(student.toString());
+
         // Create child elements for the student
         createElementWithTextContent(doc, studentElement, "FirstName", student.getFirstName());
         createElementWithTextContent(doc, studentElement, "LastName", student.getLastName());
